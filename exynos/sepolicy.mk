LineageOS/android_device_lineage_sepolicy

exynos: Add hal_lineage_livedisplay policy
Change-Id: I82c9c45d6a7e9b153063af65d7f382e33768fcc7

exynos/sepolicy.mk
@@ -1,0 +1,23 @@
+#
+# This policy configuration will be used by all exynos products
+# that inherit from Lineage
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lineage/sepolicy/exynos/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lineage/sepolicy/exynos/dynamic \
+    device/lineage/sepolicy/exynos/system
+else
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/exynos/dynamic \
+    device/lineage/sepolicy/exynos/vendor
+endif

AICP/device_aicp_sepolicy

exynos/sepolicy.mk
@@ -1,0 +1,23 @@
+#
+# This policy configuration will be used by all exynos products
+# that inherit from Lineage
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aicp/sepolicy/exynos/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aicp/sepolicy/exynos/dynamic \
+    device/aicp/sepolicy/exynos/system
+else
+BOARD_SEPOLICY_DIRS += \
+    device/aicp/sepolicy/exynos/dynamic \
+    device/aicp/sepolicy/exynos/vendor
+endif